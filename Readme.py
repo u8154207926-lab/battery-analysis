49aa41fd27c3ae6d953269abb7abe6b046ff83ad
u8154207926-lab <u8154207926@gmail.com>
2026-04-26 20:09:47 +0200

    Add files via upload

---
+++ Readme.py
@@ -0,0 +1,23 @@
+# Battery Data Analysis
+#This project analyzes battery cycling data using Python.
+## Features
+#Data cleaning
+#Capacity fade calculation
+## Tools
+#Python
+#pandas
+#matplotlib
+## How to run
+#1. Create and Put your Excel file in the  data folder:
+# Example:
+# "C:\Enrico\New\Python\23.04.2026 2\Batteries analysis\data" ,or you create your own folder 
+# and change the following folders input folder and output folder
+##2. Ordner festlegen
+# Eingabe- und Ausgabeordner festlegen
+#input_folder = r"C:\Batteries analysis\data" #wo die Excel-Datei liegt
+#output_folder = r"C:\Batteries analysis\Output" #wo Ergebnisse gespeichert werden ;r"..." = raw string → wichtig für \ in Windows 
+#or another solution
+##input_folder = r"paste your input file path here" #wo die Excel-Datei liegt
+#output_folder = r"C:\Batteries analysis\Output" #wo Ergebnisse gespeichert werden ;r"..." = raw string → wichtig für \ in Windows
+#3. Run analysis 2.py
+#4. Check results in output folder: "C:\Enrico\New\Python\23.04.2026 2\Batteries analysis\Output"
